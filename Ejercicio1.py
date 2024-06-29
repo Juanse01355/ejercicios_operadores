983fe780b03363f7de4a0b3f500444ae47795edc
juan sebastian guzman <juanse1355@gmail.com>
2024-06-29 13:51:27 -0500

    ejercicio operadores

---
+++ Ejercicio1.py
@@ -0,0 +1,49 @@
+name=input("Ingresa tu nombre:")
+#Aqui estoy haciendo una suma
+print(f"Hola, {name}  vamos a realizar una suma de enteros")
+num1=int(input(f"{name} porfavor ingresa el primer numero:"))
+num2=int(input(f"{name} porfavor ingresa segundo numero:"))
+resultado_suma= num1 + num2
+print(f"{name} el resultado de tú suma es:{resultado_suma}")
+
+#Aqui estoy haciendo una resta
+print("Muy bien vamos a continuar con la resta")
+num1=int(input(f"{name} porfavor ingresa el primer numero:"))
+num2=int(input(f"{name} porfavor ingresa segundo numero:"))
+resultado_resta= num1 - num2
+print(f"{name} el resultado de tú resta es:{resultado_resta}")
+
+#Aqui estoy haciendo una multiplicación
+print("Muy bien vamos a continuar con la multiplicacion")
+num1=int(input(f"{name} porfavor ingresa el primer numero:"))
+num2=int(input(f"{name} porfavor ingresa segundo numero:"))
+resultado_multiplicacion= num1 * num2
+print(f"{name} el resultado de tú multiplicación es:{resultado_multiplicacion}")
+
+#Aqui estoy haciendo una division
+print("Muy bien vamos a continuar con una division")
+num1=int(input(f"{name} porfavor ingresa el primer numero:"))
+num2=int(input(f"{name} porfavor ingresa segundo numero:"))
+resultado_division= num1 / num2
+print(f"{name} el resultado de tú division es:{resultado_division}")
+
+#Aqui estoy haciendo una division Entera
+print("Muy bien vamos a continuar con una division entera,es decir no se mostraran decimales en el resultado")
+num1=int(input(f"{name} porfavor ingresa el primer numero:"))
+num2=int(input(f"{name} porfavor ingresa segundo numero:"))
+resultado_division_entera= num1 // num2
+print(f"{name} el resultado de tú division entera es:{resultado_division_entera}")
+
+#Aqui estoy haciendo una operacion para sacar el modulo en una division
+print("Muy bien vamos a continuar con una division,pero el relsultado te mostrara el modulo de la division")
+num1=int(input(f"{name} porfavor ingresa el primer numero:"))
+num2=int(input(f"{name} porfavor ingresa segundo numero:"))
+resultado_modulo_de_division= num1 % num2
+print(f"{name} el resultado de tú division en forma de modulo es:{resultado_modulo_de_division}")
+
+#Aqui vamos a sacar la potencia de un numero entero
+print("Muy bien a continuacion vamos a elevar un numero a la potencia que quieras")
+num1=int(input(f"{name} porfavor ingresa el primer numero:"))
+num2=int(input(f"{name} porfavor ingresa a que potencia quieres elevar el anterior numero:"))
+resultado_potencia= num1 ** num2
+print(f"{name} el resultado de tú potencia es:{resultado_potencia}")
